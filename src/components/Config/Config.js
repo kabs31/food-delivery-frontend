e37ebde9f9ebf753f69de34aecfc59e9a68996f8
kabs31 <kabilanitech@gmail.com>
2024-06-27 03:32:55 +0530

    Update config.js

--- src/components/Config/Config.js
+++ src/components/Config/Config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    BASE_URL: 'gentle-charm-production.up.railway.app',
+    BASE_URL: 'https://gentle-charm-production.up.railway.app',
   };
   
   export default config;
